48ba8435e20ef83d20eae6c82d26b83fb2ad11fc
defr0stt <dima.sb713@gmail.com>
2024-05-21 15:05:05 +0300

    Added full reg+logIn support

--- bootsrap/assets/js/register-page.js
+++ bootsrap/assets/js/register-page.js
@@ -70,3 +70,39 @@ document.addEventListener('DOMContentLoaded', function () {
 fetchLocations()
 
 let locationsGlobal;
+
+// ======================================================================
+// POST FORM
+// ======================================================================
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form data
+    const formData = new FormData(this);
+
+    // Convert form data to JSON
+    const jsonObject = {};
+    formData.forEach((value, key) => {
+        jsonObject[key] = value;
+    });
+    const jsonData = JSON.stringify(jsonObject);
+
+    // Send POST request with JSON content type
+    fetch('http://localhost:8181/api/v1/auth/sign-up', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle response data
+            sessionStorage.setItem('accessToken', data.accessToken);
+            window.location.assign('user-page.html');
+        })
+        .catch(error => {
+            console.error('Error:', error); // Handle errors
+        });
+});
