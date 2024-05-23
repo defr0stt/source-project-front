a64d89b51e79a14a0454a49c0532c5fe7d0b9fa2
defr0stt <dima.sb713@gmail.com>
2024-05-24 01:14:14 +0300

    Added change user details

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,21 @@
 
 N
 bootsrap/assets/img/rocket.svg,3\5\3528d37857259e40eacb30cb4ce51d3afb07ccc5
+O
+bootsrap/assets/js/main-page.js,b\0\b046c93d1dbb24dbd086d09433f0eb205627b136
+T
+$bootsrap/assets/js/main-find-page.js,6\5\65fce84cbad88c7cd0ddeedb73a41b29eb5f91b7
+R
+"bootsrap/parcels-by-user-page.html,f\f\ff36151591930d980c818026061f5322e16c265e
+O
+bootsrap/track-parcel-page.html,e\d\ed70dc4e5a265fe1d6ff5a0063d9166d45a4f42a
+N
+bootsrap/assets/css/styles.css,0\b\0ba94ac9d15cb90724b833aeb427184a6a661fb5
+S
+#bootsrap/assets/js/register-page.js,8\e\8e6675e3d8611c86ccbf917e75ef735b0eb35ce1
+H
+bootsrap/login-page.html,b\c\bc6267ecfcfc203d2c6fb0ee9e2e1f0940146f3a
+K
+bootsrap/about-us-page.html,5\2\52e57e7c361950e44004ed260bc16eab530378a0
+L
+bootsrap/main-find-page.html,1\b\1b88ffdc8e148928f5091cfc3d477196d83df47c
--- bootsrap/assets/js/user-page.js
+++ bootsrap/assets/js/user-page.js
@@ -9,6 +9,10 @@ const userCountry = document.getElementById('user-country');
 const userCity = document.getElementById('user-city');
 const userRole = document.getElementById('user-role');
 
+const userData = document.getElementById('show-user-data');
+
+let backButton = '';
+let UserFullInfo = '';
 
 async function fetchUserData() {
     try {
@@ -20,6 +24,7 @@ async function fetchUserData() {
         }
         var jsonResponse = new Map(Object.entries(await response.json()));
         console.log(jsonResponse);
+        UserFullInfo = jsonResponse;
 
         let fullUsername = jsonResponse.get("firstName") + ' ' + jsonResponse.get("lastName");
         let currentUserEmail = jsonResponse.get("email");
@@ -44,4 +49,113 @@ fetchUserData();
 function logOut() {
     sessionStorage.removeItem('accessToken');
     window.location.assign('login-page.html');
+}
+
+function changeData() {
+    backButton = userData.outerHTML;
+    userData.innerHTML =
+        '<div class="col-md-14">\n' +
+        '                <div class="card mb-3">\n' +
+        '                    <div class="card-body">\n' +
+        '                        <div class="row">\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example1m" class="form-control form-control-sm" placeholder="Мартин" name="firstName" value="' + UserFullInfo.get("firstName") +'"/>\n' +
+        '                                          <label class="form-label" for="form3Example1m"> Ім\'я </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example1n" class="form-control form-control-sm" placeholder="Боруля" name="lastName" value="' + UserFullInfo.get("lastName") +'"/>\n' +
+        '                                          <label class="form-label" for="form3Example1n"> Прізвище </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                        </div>\n' +
+        '                        <div class="row">\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example1m" class="form-control form-control-sm" placeholder="martin@gmail.com" name="email" value="' + UserFullInfo.get("email") +'"/>\n' +
+        '                                          <label class="form-label" for="form3Example1m"> Email </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example1n" class="form-control form-control-sm" placeholder="123qwerty" name="password"/>\n' +
+        '                                          <label class="form-label" for="form3Example1n"> Пароль </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                        </div>\n' +
+        '                        <div class="row">\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example1m" class="form-control form-control-sm" placeholder="380521267858" name="phoneNumber" value="' + UserFullInfo.get("phoneNumber") +'"/>\n' +
+        '                                          <label class="form-label" for="form3Example1m"> Номер телефону </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <div class="form-outline">\n' +
+        '                                          <input type="text" id="form3Example19m" class="form-control form-control-sm" placeholder="https:img/123/123..." name="imageUrl" value="' + UserFullInfo.get("imageUrl") +'"/>\n' +
+        '                                          <label class="form-label" for="form3Example19m"> URL-адреса зображення </label>\n' +
+        '                                       </div>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="col-md-6 mb-4">\n' +
+        '                                       <select class="form-control" name="country" id="reg-country">\n' +
+        '                                          <option value="Україна">Україна</option>\n' +
+        '                                       </select>\n' +
+        '                                       <label class="form-label"> Країна </label>\n' +
+        '                                    </div>\n' +
+        '                                       <div class="col-md-6 mb-4">\n' +
+        '                                       <select class="form-control" name="city" id="reg-country">\n' +
+        '                                          <option value="Київ">Київ</option>\n' +
+        '                                          <option value="Вінниця">Вінниця</option>\n' +
+        '                                          <option value="Львів">Львів</option>\n' +
+        '                                       </select>\n' +
+        '                                       <label class="form-label"> Місто </label>\n' +
+        '                                    </div>\n' +
+        '                        </div>\n' +
+        '                        <div class="row">\n' +
+        '                            <div class="col-sm-12">\n' +
+        '                                <a class="btn bg-custom-grey text-dark" target="__blank" onclick="backButtonAction()" href="#">Назад</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n' +
+        '                                <a class="btn bg-custom-purple text-white" onclick="sendChangedData()" href="#">Змінити дані</a>\n' +
+        '                            </div>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>'
+}
+
+function backButtonAction() {
+    userData.outerHTML = backButton;
+}
+
+function sendChangedData() {
+    // Get form data
+    const formData = new FormData(document.querySelector('form'));
+
+    // Convert form data to JSON
+    const jsonObject = {};
+    formData.forEach((value, key) => {
+        jsonObject[key] = value;
+    });
+    jsonObject['id'] = UserFullInfo.get("id");
+    const jsonData = JSON.stringify(jsonObject);
+    console.log(jsonObject);
+
+    // Send POST request with JSON content type
+    fetch('http://localhost:8181/api/v1/user/' + UserFullInfo.get("id"), {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            sessionStorage.removeItem('accessToken');
+            window.location.assign('login-page.html');
+        })
+        .catch(error => {
+            console.error('Error:', error); // Handle errors
+        });
 }
